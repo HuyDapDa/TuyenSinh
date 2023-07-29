ca1bacd5ddbe534fc9583e1ab79e0357dba80d16
Tran Huy <quanghuytran0204@gmail.com>
2023-07-29 11:29:42 +0700

    update con

--- TuVanTuyenSinh/target/generated-sources/annotations/com/tqh/pojo/Baidang_.java
+++ TuVanTuyenSinh/target/generated-sources/annotations/com/tqh/pojo/Baidang_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-29T09:18:17")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-29T10:35:14")
 @StaticMetamodel(Baidang.class)
 public class Baidang_ { 
 
